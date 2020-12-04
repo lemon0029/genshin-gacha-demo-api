6b8706a083fcdcc2510e7c3b7aba464aa120e136
imtin <yec@arch-r.localdomain>
2020-12-04 17:01:45 +0800

    first commit

---
+++ src/main/java/me/yec/GenshinGachaDemoApiApplication.java
@@ -0,0 +1,13 @@
+package me.yec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinGachaDemoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenshinGachaDemoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/yec/GenshinGachaDemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenshinGachaDemoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
