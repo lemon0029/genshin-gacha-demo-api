3bb8d557b118d8c7e01942f94367f4f2e8a72c94
imtin <imoyb@qq.com>
2020-12-04 17:59:00 +0800

    auto commit

---
+++ src/main/java/me/yec/config/MihoyoProperties.java
@@ -0,0 +1,17 @@
+package me.yec.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yec
+ * @date 12/4/20 5:54 PM
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "mihoyo")
+public class MihoyoProperties {
+    private String accountID;
+    private String cookieToken;
+}
---
+++ src/test/java/me/yec/MihoyoPropertiesTest.java
@@ -0,0 +1,23 @@
+package me.yec;
+
+import me.yec.config.MihoyoProperties;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author yec
+ * @date 12/4/20 5:55 PM
+ */
+@SpringBootTest
+public class MihoyoPropertiesTest {
+
+    @Autowired
+    private MihoyoProperties mihoyoProperties;
+
+    @Test
+    void simpleTest() {
+        System.out.println(mihoyoProperties.getAccountID());
+        System.out.println(mihoyoProperties.getCookieToken());
+    }
+}
